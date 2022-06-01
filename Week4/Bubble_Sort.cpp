2b6ec93fd9028521424cbb6195d0234b82840130
Ben Bullard <benjaminandrewbullard@gmail.com>
2022-05-31 18:08:23 -0700

    bubble

--- Week4/Bubble_Sort.cpp
+++ Week4/Bubble_Sort.cpp
@@ -10,6 +10,12 @@ mt19937 generator(time(NULL));
 
 int main() {
     vector<int> numList;
+    uniform_int_distribution<int> Uniform(0,100);
+
+    for(int i = 0;i < 100;i++){
+        numList.push_back(Uniform(generator));
+
+    }
 
 
 }
