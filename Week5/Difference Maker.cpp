88191cb6db06604b6f56ee0df8cd7bd7821baa4c
Ben Bullard <benjaminandrewbullard@gmail.com>
2022-06-03 17:28:10 -0700

    work

--- Week5/Difference Maker.cpp
+++ Week5/Difference Maker.cpp
@@ -1,18 +1,71 @@
 #include<iostream>
-#include<random>
-#include<time.h>
 #include<vector>
+#include<math.h>
 
 using namespace std;
 
-mt19937 generator(time(NULL));
-
-
+void BubbleSort(vector<int> &);
 int main() {
+
     vector<int> numList;
+
+
+    int userNumber;
+
+    int Hnumber;
+    int Lnumber;
+
+    int FHnumber;
+    int FLnumber;
+    int Rvalue = 1000;
+
+    for(int i = 0; i < 10; i++){
+
+        cout << "enter a number('101' to stop): ";
+        cin >> userNumber;
+
+        if(userNumber == 101){
+            break;
+        }
+
+        numList.push_back(userNumber);
+    }
+
+    BubbleSort(numList);
+
+    for(int i = numList.size(); i > 0; i--) {
+
+        Hnumber = numList[i];
+        Lnumber = numList[i - 1];
+
+        if(Hnumber - Lnumber < Rvalue) {
+
+            FHnumber = Hnumber;
+            FLnumber = Lnumber;
+            Rvalue = Hnumber - Lnumber;
+
+        }
+    }
+
+    cout << "difference between " << FHnumber << " & " << FLnumber << " is: " << Rvalue << endl;
 
 
 }
 
 
+void BubbleSort (vector<int> &theList) {
 
+    bool swapped = true;
+
+    while(swapped){
+        swapped = false;
+        for(int t = 1; t < theList.size(); t++){
+            if(theList[t] < theList[t-1]){
+                int temp = theList[t];
+                theList[t] = theList[t-1];
+                theList[t-1] = temp;
+                swapped = true;
+            }
+        }
+    }
+}
