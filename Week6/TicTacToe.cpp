d028fee6f544177a10c87a2726c8445a8e76773d
Ben Bullard <benjaminandrewbullard@gmail.com>
2022-06-10 22:00:53 -0700

    tiktak_DONE

--- Week6/TicTacToe.cpp
+++ Week6/TicTacToe.cpp
@@ -16,23 +16,38 @@ int main() {
     vector<int>  gameBoard = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     vector<char>  gameBoardchar = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+    boardDraw(gameBoard, gameBoardchar);
     while(true){
-        boardDraw(gameBoard, gameBoardchar);
 
         cout << "Player 1 Move: ";
         cin >> P1;
 
         gameBoard[P1 - 1] = 10;
-
-
-
-
         boardDraw(gameBoard, gameBoardchar);
+
+        if(winner(gameBoard) == 1){
+            cout << "Player 1 has won!!" << endl;
+            break;
+        }else if(winner(gameBoard) == 2){
+            cout << "Player 2 has won!!" << endl;
+            break;
+        }
+
+
 
         cout << "Player 2 Move: ";
         cin >> P2;
 
         gameBoard[P2 - 1] = 11;
+        boardDraw(gameBoard, gameBoardchar);
+
+        if(winner(gameBoard) == 1){
+            cout << "Player 1 has won!!" << endl;
+            break;
+        }else if(winner(gameBoard) == 2){
+            cout << "Player 2 has won!!" << endl;
+            break;
+        }
 
     }
 }
@@ -100,13 +115,42 @@ void boardDraw (vector<int> Board, vector<char> BoardChar){
 
 
 int winner(vector<int> Board){
-//Their is probably an actual mathematical way to do this, but idk what it is so im going to do it like this. I call it "the long way".
-//this function must figure out who has actualy won. It is going to check ALL of the different winning combos that are possible for BOTH players.
 
-//    if(Board[]){
-//    return 1;
-//    else if(){
-//    return 2;
-}
+    if(Board[0] == 10 && Board[1] == 10 && Board[2] == 10){
+        return 1;
+    }else if(Board[3] == 10 && Board[4] == 10 && Board[5] == 10){
+        return 1;
+    }else if(Board[6] == 10 && Board[7] == 10 && Board[8] == 10){
+        return 1;
+    }else if(Board[0] == 10 && Board[3] == 10 && Board[6] == 10){
+        return 1;
+    }else if(Board[1] == 10 && Board[4] == 10 && Board[7] == 10){
+        return 1;
+    }else if(Board[2] == 10 && Board[5] == 10 && Board[8] == 10){
+        return 1;
+    }else if(Board[0] == 10 && Board[4] == 10 && Board[8] == 10){
+        return 1;
+    }else if(Board[2] == 10 && Board[4] == 10 && Board[6] == 10){
+        return 1;
+
+    }else if(Board[0] == 11 && Board[1] == 11 && Board[2] == 11){
+        return 2;
+    }else if(Board[3] == 11 && Board[4] == 11 && Board[5] == 11){
+        return 2;
+    }else if(Board[6] == 11 && Board[7] == 11 && Board[8] == 11){
+        return 2;
+    }else if(Board[0] == 11 && Board[3] == 11 && Board[6] == 11){
+        return 2;
+    }else if(Board[1] == 11 && Board[4] == 11 && Board[7] == 11){
+        return 2;
+    }else if(Board[2] == 11 && Board[5] == 11 && Board[8] == 11){
+        return 2;
+    }else if(Board[0] == 11 && Board[4] == 11 && Board[8] == 11){
+        return 2;
+    }else if(Board[2] == 11 && Board[4] == 11 && Board[6] == 11){
+        return 2;
+    }else{
+        return 0;
+    }
 }
 
